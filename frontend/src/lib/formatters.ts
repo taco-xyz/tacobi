7b3aa5fe0a84666aa781b2c74ce1c6ffaac44417
Margarida Oliveira <margarida.o.2003@gmail.com>
2025-04-21 14:14:44 +0400

    chore: Remove unnecessary files

--- frontend/src/lib/formatters.ts
+++
@@ -1,7 +0,0 @@
-export const currencyFormatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-  notation: "compact",
-  minimumFractionDigits: 0,
-  maximumFractionDigits: 0,
-});
